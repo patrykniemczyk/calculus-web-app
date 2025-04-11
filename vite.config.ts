683ccb489c34ed77013bf4a848eff45efb20d73e
Patryk <patryk.niemczyk12345@gmail.com>
2025-04-11 23:18:20 +0200

    Changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/calculus-web-app/',
   plugins: [
     react(),
     tailwindcss(),
